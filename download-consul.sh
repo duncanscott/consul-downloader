67d75844bd893998b4eb477339c86fb46fc09de0
Consul Couch-Dev <dnscott@lbl.gov>
2021-07-14 12:20:48 -0700

    create download dir after verifying

--- download-consul.sh
+++ download-consul.sh
@@ -24,14 +24,13 @@ fi
 echo "downloading Consul version $CONSUL_VERSION"
 
 set -e
+
 downloaddir="${HOME}/downloads"
 dirname=consul_"$CONSUL_VERSION"
 dirpath="$downloaddir/consul/$dirname"
 tmppath=/tmp/$dirname
-[ -d "$dirpath" ] && \rm -rf "$dirpath"
 [ -d "$tmppath" ] && \rm -rf "$tmppath"
 mkdir -p "${tmppath}"
-mkdir -p "${dirpath}"
 cd $tmppath
 
 curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
@@ -39,6 +38,7 @@ curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSI
 curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS.sig
 
 set +e
+
 shasum=`sha256sum consul_${CONSUL_VERSION}_linux_amd64.zip`
 echo "verifying sha256sum: ${shasum}"
 grep -q "${shasum}" consul_${CONSUL_VERSION}_SHA256SUMS
@@ -51,6 +51,9 @@ else
 fi
 
 set -e
+
+[ -d "$dirpath" ] && \rm -rf "$dirpath"
+mkdir -p "${dirpath}"
 unzip consul_${CONSUL_VERSION}_linux_amd64.zip
 mv * "${dirpath}"
 echo "files downloaded to ${dirpath}"
